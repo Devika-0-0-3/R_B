65a72a6b13153cb28e78fe29e98423aa1a292dae
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-06-06 01:15:04 +0530

    Link vs NavLink

--- React-Router/src/components/Navbar.jsx
+++ React-Router/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import react from '../assets/react.svg'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+
+/*Link or NavLink it will be converted to anchor tag <a>*/
+/* Using NavLink instead of Link allows us to apply styles to the active link */ 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={react} alt=''/>
       <ul className='nav-links'>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/products'><li>Products</li></Link>
-        <Link to='/about'><li>About</li></Link>
-        <Link to='/contact'><li>Contact</li></Link>
+        <NavLink to='/'><li>Home</li></NavLink>
+        <NavLink to='/products'><li>Products</li></NavLink>
+        <NavLink to='/about'><li>About</li></NavLink>
+        <NavLink to='/contact'><li>Contact</li></NavLink>
       </ul>
       <button>Get Started</button>
     </div>
