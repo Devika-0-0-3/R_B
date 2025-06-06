ecc5d21783bf66a8f56126fe7a225022e3cf16cc
Devika-0-0-3 <mailtodevika2003@gmail.com>
2025-06-06 08:39:54 +0530

    Page Not Found

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -8,6 +8,7 @@ import ContactInfo from './components/ContactInfo'
 import ContactForm from './components/ContactForm'
 import RootLayout from './layout/RootLayout' 
 import ContactLayout from './layout/ContactLayout'
+import NotFound from './components/NotFound'
 
 const App = () => { 
   const router=createBrowserRouter(
@@ -19,9 +20,8 @@ const App = () => {
           <Route path='contact' element={<ContactLayout/>}>
             <Route path='info' element={<ContactInfo/>}/>
             <Route path='form' element={<ContactForm/>}/>
-          
-          
           </Route>
+          <Route path='*' element={<NotFound/>}/>
       </Route>
     )
   )
